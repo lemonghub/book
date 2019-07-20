cf35524ec3e359846d43b025f9771ce74e4d1cd0
lemonghub <qinping_email@163.com>
2019-07-20 15:09:46 +0800

    初始化

---
+++ book/src/main/java/com/jmu/book/controller/HelloController.java
@@ -0,0 +1,14 @@
+package com.jmu.book.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+public class HelloController {
+
+    @RequestMapping("/hello")
+    public String hello() {
+        return "Hello Spring Boot!";
+    }
+
+}
