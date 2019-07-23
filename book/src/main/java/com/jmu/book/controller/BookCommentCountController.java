d5208491c1ecb2e2053b7daa69f4e7ee9cda7510
18950180450 <chenzhen_c@163.com>
2019-07-23 15:47:08 +0800

    图书评论数分析新增后端代码

--- book/src/main/java/com/jmu/book/controller/BookCommentCountController.java
+++ book/src/main/java/com/jmu/book/controller/BookCommentCountController.java
@@ -9,6 +9,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.awt.print.Book;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -20,21 +21,35 @@ public class BookCommentCountController {
     @Autowired
     private BookBigService bookBigService;
 
+    //查所有年份和图书类别
     @RequestMapping("/findYearAndCategory1")
-    @ResponseBody
     public String findYearAndCategory1(){
         List<String> list1 = bookBigService.findAllYear();
         List<String> list2 = bookBigService.findAllCategory();
         List<List<String>> list = new ArrayList<>();
         list.add(list1);
         list.add(list2);
-        return BookBigController.ListToJson(list);
+        return "findByYearOrCategory";
     }
 
+    //按照年份或者图书类别查图书评论数
     @RequestMapping("/findByYearOrCategory")
     @ResponseBody
     public String findByYearOrCategory(Model model, String year, String category){
         List<BookCommentCount> list = bookCommentCountService.findByYearOrCategory(year, category);
+        return BookBigController.ListToJson(list);
+    }
+
+    //所有的图书评论数
+    @RequestMapping("/findAllCommentCount")
+    @ResponseBody
+    public String findAllCommentCount(){
+        List<String> list1 = bookBigService.findAllCategory();
+        List<List<BookCommentCount>> list = new ArrayList<>();
+        for (String s : list1) {
+            List<BookCommentCount> list2 = bookCommentCountService.findByYearOrCategory(null,s);
+            list.add(list2);
+        }
         return BookBigController.ListToJson(list);
     }
 }
